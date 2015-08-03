669405553a0b1cce36524c8d75b58386c34d7be4
cjgalvez <carlosgalvez@MBP-de-Carlos.Home>
2015-08-03 12:33:59 +0200

    Despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,3 +16,22 @@ exports.answer = function(req, res) {
 exports.author = function(req, res) {
   res.render('other/author');
 }
+
+var models = require('../models/models.js');
+
+exports.question = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta});
+  });
+};
+
+exports.answer = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto'});
+    }
+    else {
+      res.render('quizes/answer', { respuesta: 'Incorrecto'});
+    }
+  })
+};
